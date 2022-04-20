e90a6bd6ee5d603876dd39959c279dbcbf2267db
KyeRox <kwykyaw.99@gmail.com>
2022-04-20 14:25:16 +0630

    v02: DB created, DB INSERT completed

--- api/root.ctrl.js
+++ api/root.ctrl.js
@@ -1,13 +1,33 @@
+const mysqlPool = require('./database/mysql_db.js');
+
 const control = {
   rootGet: (req, res) => {
     console.log(req);
-    res.status(201).send("Hello from Home");
+    res.status(201).send('Hello from Home');
   },
   rootPost: (req, res) => {
-    console.log(req);
-    console.log(res);
-    // res.status(201).send(`Hello ${req.body.username}`);
+    mysqlPool.getConnection((err, conn) => {
+      if (!err) {
+        query = `INSERT INTO info (username, password) VALUES ?`;
+        values = [[req.body.userName, req.body.passWord]];
+        const c = conn.query(query, [values], (err, rows) => {
+          if (!err) {
+            res.status(201).send({ message: 'Successfully Created' });
+          } else {
+            res
+              .status(403)
+              .send({ message: 'Record Creation Failed', error: err.message });
+          }
+        });
+        console.log(req.body);
+        console.log(c);
+        conn.release();
+      } else {
+        res
+          .stauts(500)
+          .send({ message: 'Internal Server Error', error: err.message });
+      }
+    });
   },
 };
-
 module.exports = control;
--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
-import React, { useState } from "react";
-import "./App.css";
-import Axios from "./axios";
+import React, { useState } from 'react';
+import './App.css';
+import Axios from './axios';
 
 function App() {
-  const [userName, setUsername] = useState("");
-  const [passWord, setPassword] = useState("");
+  const [userName, setUsername] = useState('');
+  const [passWord, setPassword] = useState('');
 
   const save = () => {
     const result = {
@@ -13,41 +13,41 @@ function App() {
       passWord: passWord,
     };
 
-    Axios.post("http://localhost:3000/", result)
-      .then((req) => {
-        console.log(req);
+    Axios.post('/', result)
+      .then((res) => {
+        console.log(res);
       })
       .catch((err) => {
         console.log(err);
       });
   };
   return (
-    <div className="form">
-      <div className="form__content">
-        <div className="form__header">
+    <div className='form'>
+      <div className='form__content'>
+        <div className='form__header'>
           <p>First React App</p>
         </div>
-        <div className="form__body">
-          <div className="form__row">
-            <div className="form__info row_2">
+        <div className='form__body'>
+          <div className='form__row'>
+            <div className='form__info row_2'>
               <label>Username</label>
               <input
-                type="text"
-                placeholder="Type Username"
+                type='text'
+                placeholder='Type Username'
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
-            <div className="form__info row_2">
+            <div className='form__info row_2'>
               <label>Password</label>
               <input
-                type="text"
-                placeholder="Type Password"
+                type='password'
+                placeholder='Type Password'
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
-          <div className="form__row footer">
-            <button id="save" onClick={() => save()}>
+          <div className='form__row footer'>
+            <button id='save' onClick={() => save()}>
               SAVE
             </button>
           </div>
